0ed2e5103e5aca9513d642bca06669e03d76c663
Ruy Guzman Camacho <ruy.guzman2002@gmail.com>
2021-12-01 10:43:00 -0600

    chart template

--- functions.js
+++ functions.js
@@ -1,11 +1,14 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("myChart").style.marginLeft = "275px";
 }
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "25%";
+    document.getElementById("myChart").style.marginLeft = "25%";
+
 }
 
 
